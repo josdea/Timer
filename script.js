fce84231484ff6f7645d81477d551dcd583f93d7
Josh Dean <josdea@users.noreply.github.com>
2020-06-29 12:52:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -45,3 +45,12 @@ setInterval(countdownTimer, 1000);
 // TODO preload video and prompt after loading
 // numbers are clickable to restart or update time
 // use bootstrap modals instead of alert prompts
+
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-73531957-1', 'auto');
+  ga('send', 'pageview');
